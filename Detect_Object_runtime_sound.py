9345a11ce323198b5a78f75cf39294e8c5115143
Sharique1231 <shariquepervez2000@gmail.com>
2022-10-04 12:34:59 +0530

    Add files via upload

---
+++ Detect_Object_runtime_sound.py
@@ -0,0 +1,86 @@
+import cv2
+from time import sleep
+import numpy as np
+from playsound import playsound
+import pyttsx3
+text_speak = pyttsx3.init()
+
+net = cv2.dnn.readNet('yolov3.weights','yolov3.cfg')
+classes = []
+with open('coco.names','r') as f:
+    classes = f.read().splitlines()
+
+#print(classes)
+cap = cv2.VideoCapture(0)
+#img = cv2.imread('dog.jpg')
+sleep(2)
+while True:
+    _, img = cap.read()
+    height,width,_ = img.shape
+
+    blob = cv2.dnn.blobFromImage(img, 1/255, (416,416), (0,0,0), swapRB = True, crop = False)
+    net.setInput(blob)
+    output_layers_names = net.getUnconnectedOutLayersNames()
+    layerOutputs = net.forward(output_layers_names)
+
+    boxes = []
+    confidences = []
+    class_ids = []
+
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0]*width)
+                center_y = int(detection[1]*height)
+                w = int(detection[2]*width)
+                h = int(detection[3]*height)
+
+                x = int(center_x - w/2)
+                y = int(center_y - h/2)
+
+                boxes.append([x,y,w,h])
+                confidences.append((float(confidence)))
+                class_ids.append(class_id)
+
+    #print(len(boxes))
+    indexes = cv2.dnn.NMSBoxes(boxes,confidences,0.5,0.4)
+    #print(indexes.flatten())
+    font = cv2.FONT_HERSHEY_PLAIN
+    colors = np.random.uniform(0,255,size=(len(boxes),3))
+   
+    for i in indexes.flatten():
+        x,y,w,h = boxes[i]
+        label = str(classes[class_ids[i]])
+        confidence = str(round(confidences[i],2))
+        color = colors[i]
+        cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
+        cv2.putText(img, label + "" + confidence, (x,y+20),font,2,(255,255,255),2)
+        print(label)
+        if (label) == "person":
+            text_speak.say(label)
+            text_speak.runAndWait()
+        if (label) == "key":
+            text_speak.say(label)
+            text_speak.runAndWait()
+        if (label) == "cell phone":
+            text_speak.say(label)
+            text_speak.runAndWait()
+        if (label) == "box":
+            text_speak.say(label)
+            text_speak.runAndWait()
+
+    cv2.imshow('Image',img)
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+    
+cap.release()    
+cv2.destroyAllWindows()
+
+
+
+
+
